6b345e539131d6af88887645f74fa194f07352a2
ZackoYo <zarko.y@gmail.com>
2025-07-01 15:16:49 +0300

    Add Base Entity

---
+++ API/Forum.Domain/Common/BaseEntity.cs
@@ -0,0 +1,14 @@
+namespace Forum.Domain.Common;
+
+public abstract class BaseEntity<TKey>
+{
+    public TKey Id { get; set; }
+    public DateTime CreatedOn { get; set; }
+    public DateTime? ModifiedOn { get; set; }
+}
+
+public abstract class BaseDeletableEntity<TKey> : BaseEntity<TKey>
+{
+    public bool IsDeleted { get; set; }
+    public DateTime? DeletedOn { get; set; }
+}

25880ec100a7628257a91a71a81f3ee226f40940
ZackoYo <zarko.y@gmail.com>
2025-07-02 17:17:11 +0300

    Add Category model

---
+++ API/Forum.Domain/Entities/Category.cs
@@ -0,0 +1,29 @@
+using Forum.Domain.Common;
+
+namespace Forum.Domain.Entities;
+
+public class Category : BaseDeletableEntity<int>
+{
+    public Category()
+    {
+        this.Posts = new HashSet<Post>();
+        this.SubCategories = new HashSet<Category>();
+    }
+
+    public string Name { get; set; }
+
+    public string Description { get; set; }
+
+    public string Slug { get; set; }
+
+    public int? ParentCategoryId { get; set; }
+    public Category ParentCategory { get; set; }
+
+    public virtual ICollection<Category> SubCategories { get; set; }
+
+    public virtual ICollection<Post> Posts { get; set; }
+
+    public int PostsCount { get; set; }
+
+    public int DisplayOrder { get; set; }
+}

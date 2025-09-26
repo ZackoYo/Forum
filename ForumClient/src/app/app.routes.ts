47d7b0900737ad25a154f9fff9c7d81bcb19dad8
ZackoYo <zarko.y@gmail.com>
2025-09-26 09:13:17 +0300

    Build Not-found component

--- ForumClient/src/app/app.routes.ts
+++ ForumClient/src/app/app.routes.ts
@@ -1,26 +1,27 @@
 import { Routes } from '@angular/router';
+import { NotFoundComponent } from './shared/components/not-found/not-found.component';
 
 export const routes: Routes = [
     {
     path: '',
     component: HomeComponent
   },
-  {
-    path: 'categories',
-    component: CategoriesComponent
-  },
-  {
-    path: 'categories/:slug',
-    component: CategoryPageComponent
-  },
-  {
-    path: 'posts/new',
-    component: NewPostComponent
-  },
-  {
-    path: 'posts/:slug',
-    component: PostDetailComponent
-  },
+//   {
+//     path: 'categories',
+//     component: CategoriesComponent
+//   },
+//   {
+//     path: 'categories/:slug',
+//     component: CategoryPageComponent
+//   },
+//   {
+//     path: 'posts/new',
+//     component: NewPostComponent
+//   },
+//   {
+//     path: 'posts/:slug',
+//     component: PostDetailComponent
+//   },
   {
     path: '**',
     component: NotFoundComponent
